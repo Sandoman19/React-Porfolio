d2d1b99c9a5e4e10ad5affaf6380f52c146388a9
Aaron Sando <Aaron@Charactercabinets.com.au>
2022-02-24 13:35:21 +0800

    its working now to make it look better

---
+++ src/pages/notFoundPage.js
@@ -0,0 +1,20 @@
+
+import React from "react";
+
+const notFoundPage = () => {
+  return (
+    <div>
+      <h2>
+        404 Not Found
+      </h2>
+      <div>
+        <p>
+          keep looking you might find it
+        </p>
+      </div>
+    </div> 
+  )
+}
+
+
+export default notFoundPage
---
+++ src/pages/portfolio.js
@@ -0,0 +1,119 @@
+import React from "react";
+import "../App.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { View, Image, StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    paddingTop: 50,
+  },
+  stretch: {
+    width: 300,
+    height: 200,
+    resizeMode: 'stretch',
+  },
+});
+
+const portfolio = () => {
+  return (
+    <div className="portfolioApp">
+      <div className="container">
+        <div className="card">
+          <div className="card-header">
+            <h3>
+              Portfolio
+            </h3>
+          </div>
+
+          <div className="card-body">
+            <div className="row">
+              {/* First row of homework assignments */}
+              <div className="col-md-1">
+              </div>
+              <div className="col-md-3">
+                  <a href="https://sandoman19.github.io/Password-generater/">
+                    <View style={styles.container}>
+                      <Image
+                        style={styles.stretch}
+                        source={require('../img/PasswordGenerater.png')}
+                      />
+                    </View>                                   
+                  </a>
+                  <p className="card-text" id="repo"><a href="https://github.com/Sandoman19/Password-generater/">Password Generater</a></p>
+              </div>
+
+              <div className="col-md-1"></div>
+
+              <div className="col-md-3">
+                  <a href="https://sandoman19.github.io/guessing_game//">
+                    <View style={styles.container}>
+                      <Image
+                        style={styles.stretch}
+                        source={require('../img/GuessingGame.png')}
+                      />
+                    </View>                                   
+                  </a>
+                  <p className="card-text" id="repo"><a href="https://github.com/Sandoman19/guessing_game">API Code Quiz</a></p>
+              </div>
+            </div>
+
+            <br></br>
+            <br></br>
+            <br></br>
+            {/* Second row of homework assignments */}
+            <div className="row">
+              <div className="col-md-1">
+              </div>
+              <div className="col-md-3">
+                  <a href="https://sandoman19.github.io/WorkDayScheduler/">
+                    <View style={styles.container}>
+                      <Image
+                        style={styles.stretch}
+                        source={require('../img/WorkDayScheduler.png')}
+                      />
+                    </View>                                   
+                  </a>
+                  <p className="card-text" id="repo"><a href="https://github.com/Sandoman19/WorkDayScheduler/">WorkDay Scheduler</a></p>
+              </div>
+
+              <div className="col-md-1"></div>
+
+              <div className="col-md-3">
+                  <a href="https://vast-tor-30602.herokuapp.com/">
+                    <View style={styles.container}>
+                      <Image
+                        style={styles.stretch}
+                        source={require('../img/Estore.png')}
+                      />
+                    </View>                                   
+                  </a>
+                  <p className="card-text" id="repo"><a href="https://github.com/Sandoman19/Redux-Store">Reduc-Store</a></p>
+              </div>
+            </div>
+            <br></br>
+            <br></br>
+            <div className="row">
+              
+            </div>
+            <br></br>
+            <div className="gitProfile">
+                <p>
+									GitHub Profile: <a href="https://github.com/Sandoman19"> https://github.com/Sandoman19 </a>
+								</p>
+                <p>
+									Linkedin Profile: <a href="https://www.linkedin.com/in/aaron-sandoman-sando/"> https://www.linkedin.com/in/aaron-sandoman-sando/ </a>
+								</p>
+                <p>
+									Email Address: Aaron@Charactergroup.com.au
+								</p>
+                <p>
+									Phone Number: 0423058203
+								</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+export default portfolio
